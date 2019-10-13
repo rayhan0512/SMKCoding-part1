ce7121a8dd1449e9e82bd6d840d6514a44ecbf9f
rayhan0512 <muhammadrayhan.abyansyah@gmail.com>
2019-10-13 10:24:30 +0700

    first commit

---
+++ app/src/main/java/com/example/bookingfutsal/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookingfutsal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
